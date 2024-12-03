5b3d89b310b10780b4dce907df7e9fa16be368cb
UMAIR AHMED <umairahmed51u@gmail.com>
2024-12-03 01:08:49 -0800

    Add files via upload

---
+++ Jawa-script-1/sendbox.js
@@ -0,0 +1,48 @@
+                        // Assignment-01
+
+alert("Hello")
+alert("Error! please enter a walid password")
+alert("Welcome to Js land\n Happy Coding!")
+alert("Welcome to js Land")
+alert("Hello....I can run Js through my web browsers cansole")
+
+                    // Assignment-02
+var usern
+var myName = "Umair Ahmed";
+console.log(myName)
+var message;
+message = "Hello World";
+alert(message)
+var Name = "Umair Ahmed";
+var CourseName = "Web and Mobile App Development";
+var InstituteName = "Saylani Mass IT (SMIT)";
+var Email = "Umairahmed510u@gmail.com";
+var Contact = "03118953057"
+alert("Name: " + Name);
+alert("CourseName: " + CourseName);
+alert("InstituteName: " + InstituteName);
+alert("Email: " + Email);
+alert("Mobile Number: " + Contact)
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+var Email = "Umairahmed510u@gmail.com";
+alert("My email address is : " + Email)
+var Book = "A smarter way to Learn JavaScript'";
+alert("I am trying to learn from the Book " + Book)
+document.write("Yah! I can write HTML content through JavaScript\n");
+
+document.write("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+                        // Assignment-03
+
+var age = "18 years old .";
+alert("I am " + age)
+var birthYear = 2006 ;
+document.write("My birth year is " + birthYear);
+document.write("Data type of my ddeclared variable is number");
+
+ var visitorName = " John Doe ";
+ var productTitle = " T-shirt ";
+ var Quantity = 5
+ message = visitorName + "ordered " + Quantity + productTitle + "(s) on XYZ Clothing store.";
+document.getElementById('order-message').innerText = message;
